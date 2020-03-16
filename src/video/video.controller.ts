0fb786c1add5c2d1bc254265ccefb0f8f8779bd3
Anton Tiptyuk <anton.tiptyuk@gmail.com>
2020-03-16 17:34:37 +0200

    cheater's notice

--- src/video/video.controller.ts
+++ src/video/video.controller.ts
@@ -1,6 +1,18 @@
 import { Controller, Post, UploadedFile, UseInterceptors } from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express';
 
+// I did cheat a little bit.
+// Occasionally i've come across the repo:
+// https://github.com/jamesb3ll/mytube-react-graphql
+// and found out there's a way to hanlde file upload
+// staying within graphQL API
+
+// Trying to do something similar i stuck with the fact that currently
+// 'graphql-tools' i am using is not capable of mapping all things for
+// that upload and i gave up for now
+// https://www.apollographql.com/docs/apollo-server/data/file-uploads/
+// https://github.com/apollographql/graphql-tools/issues/671
+
 @Controller('video')
 export class VideoController {
   @UseInterceptors(FileInterceptor('file'))
