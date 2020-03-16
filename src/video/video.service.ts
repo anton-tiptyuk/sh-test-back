b2dd42547ce7f5958b361b12cd32659e684c0c3c
Anton Tiptyuk <anton.tiptyuk@gmail.com>
2020-03-16 13:12:51 +0200

    init with dummy list of videos

--- src/video/video.service.ts
+++ src/video/video.service.ts
@@ -3,6 +3,19 @@ import { Injectable } from '@nestjs/common';
 import { Video } from './video.model';
 import { NewVideoInput } from './dto';
 
+
+const mockVids: NewVideoInput[] = [
+  {
+    title: 'sample video',
+    filename: 'any',
+  },
+  {
+    title: 'another video',
+    filename: 'differentFile',
+    description: 'description is present',
+  },
+];
+
 @Injectable()
 export class VideoService {
   private idSequence: number;
@@ -10,6 +23,16 @@ export class VideoService {
   constructor() {
     this.idSequence = 0;
     this.videos = [];
+
+    this.createMocks();
+  }
+
+  async createMocks() {
+    let i = 0;
+    while (i < mockVids.length) {
+      await this.create(mockVids[i]);
+      ++i;
+    }
   }
 
   async create(data: NewVideoInput): Promise<Video> {
