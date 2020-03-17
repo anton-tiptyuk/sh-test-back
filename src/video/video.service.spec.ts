7a9607071915fe782f5e1c0fee457386c44284e8
Anton Tiptyuk <anton.tiptyuk@gmail.com>
2020-03-17 15:17:33 +0200

    inline snapshots created on the first run

--- src/video/video.service.spec.ts
+++ src/video/video.service.spec.ts
@@ -16,14 +16,46 @@ describe('video service unit test', () => {
 
   it('lists dummy videos', () => {
     const videoService = new VideoService();
-    expect(videoService.findAll()).resolves.toMatchInlineSnapshot();
+    expect(videoService.findAll()).resolves.toMatchInlineSnapshot(`
+      Array [
+        Object {
+          "creationDate": 2020-03-17T13:02:16.000Z,
+          "filename": "any",
+          "id": "1",
+          "path": "wrongPath",
+          "thumbnailPath": "wrongThumbPath",
+          "title": "sample video",
+        },
+        Object {
+          "creationDate": 2020-03-17T13:02:16.000Z,
+          "description": "description is present",
+          "filename": "differentFile",
+          "id": "2",
+          "path": "wrongPath",
+          "thumbnailPath": "wrongThumbPath",
+          "title": "another video",
+        },
+      ]
+    `);
   });
 
   it('removes videos', async () => {
     const videoService = new VideoService();
     expect(await videoService.remove('1')).toBe(true);
     expect(await videoService.remove('1')).toBe(false);
-    expect(videoService.findAll()).resolves.toMatchInlineSnapshot();
+    expect(videoService.findAll()).resolves.toMatchInlineSnapshot(`
+      Array [
+        Object {
+          "creationDate": 2020-03-17T13:02:16.000Z,
+          "description": "description is present",
+          "filename": "differentFile",
+          "id": "2",
+          "path": "wrongPath",
+          "thumbnailPath": "wrongThumbPath",
+          "title": "another video",
+        },
+      ]
+    `);
   });
 
   it('adds videos', async () => {
@@ -36,15 +68,74 @@ describe('video service unit test', () => {
     };
 
     const videoService = new VideoService();
-    expect(videoService.create(fakeInput)).toMatchInlineSnapshot();
-    expect(await videoService.findAll()).toMatchInlineSnapshot();
+    expect(videoService.create(fakeInput)).toMatchInlineSnapshot(`
+      Object {
+        "creationDate": 2020-03-17T13:02:16.000Z,
+        "description": "fake video description",
+        "filename": "test filename",
+        "id": "3",
+        "path": "fake/upload/path",
+        "thumbnailPath": "fake/thumb/path",
+        "title": "fake title",
+      }
+    `);
+    expect(await videoService.findAll()).toMatchInlineSnapshot(`
+      Array [
+        Object {
+          "creationDate": 2020-03-17T13:02:16.000Z,
+          "filename": "any",
+          "id": "1",
+          "path": "wrongPath",
+          "thumbnailPath": "wrongThumbPath",
+          "title": "sample video",
+        },
+        Object {
+          "creationDate": 2020-03-17T13:02:16.000Z,
+          "description": "description is present",
+          "filename": "differentFile",
+          "id": "2",
+          "path": "wrongPath",
+          "thumbnailPath": "wrongThumbPath",
+          "title": "another video",
+        },
+        Object {
+          "creationDate": 2020-03-17T13:02:16.000Z,
+          "description": "fake video description",
+          "filename": "test filename",
+          "id": "3",
+          "path": "fake/upload/path",
+          "thumbnailPath": "fake/thumb/path",
+          "title": "fake title",
+        },
+      ]
+    `);
   });
 
   it('finds video by id', async () => {
     const videoService = new VideoService();
-    expect(await videoService.findOneById('2')).toMatchInlineSnapshot();
-    expect(await videoService.findOneById('1')).toMatchInlineSnapshot();
-    expect(await videoService.findOneById('absent entry')).toMatchInlineSnapshot();
+    expect(await videoService.findOneById('2')).toMatchInlineSnapshot(`
+      Object {
+        "creationDate": 2020-03-17T13:02:16.000Z,
+        "description": "description is present",
+        "filename": "differentFile",
+        "id": "2",
+        "path": "wrongPath",
+        "thumbnailPath": "wrongThumbPath",
+        "title": "another video",
+      }
+    `);
+    expect(await videoService.findOneById('1')).toMatchInlineSnapshot(`
+      Object {
+        "creationDate": 2020-03-17T13:02:16.000Z,
+        "filename": "any",
+        "id": "1",
+        "path": "wrongPath",
+        "thumbnailPath": "wrongThumbPath",
+        "title": "sample video",
+      }
+    `);
+    expect(
+      await videoService.findOneById('absent entry'),
+    ).toMatchInlineSnapshot(`undefined`);
   });
-
 });
